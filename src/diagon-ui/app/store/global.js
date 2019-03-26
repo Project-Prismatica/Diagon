c6f51e3eb0f1abdc6482ce83db352d2a17af35f7
0sm0s1z <mtoussain@gmail.com>
2019-03-25 19:32:43 -0500

    Diagon UI update 0.1.0

--- src/diagon-ui/app/store/global.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const SessionContext = React.createContext({});
-
-export const SessionProvider = SessionContext.Provider;
-export const SessionConsumer = SessionContext.Consumer;
